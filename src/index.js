2bd5984201788d6ed423b58ddfcf732fcca346fd
Ting Zhou <limtingzhou@gmail.com>
2019-01-05 01:02:25 +0800

    Creates react comonent and renders

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+    return "CLICK ME PLEASE";
+}
+
+
 // Create a react component
 const App = function() {
-    return <div>Hi there!</div>;
+    const buttonText = {text: "Click me!"};
+
+
+    return (
+        <div> 
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
